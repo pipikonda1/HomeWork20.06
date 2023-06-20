47db91e2a6f4ae99723d39fd4fc3f7972a1e046f
kuninja <ilya.stasevich.98@gmail.com>
2023-06-20 20:13:27 +0300

    HomeWork

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int absNumber = Math.Abs(number);
+
+        int numDigits = (int)Math.Floor(Math.Log10(absNumber) + 1);
+
+        if (numDigits >= 3)
+        {
+            int thirdDigit = (absNumber / 100) % 10;
+            Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine($"В числе {number} меньше трех цифр");
+        }
---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Write("Введите цифру обозначающую день недели: ");
+int Num = int.Parse(Console.ReadLine());
+if (Num > 0 && Num < 5)
+{
+    Console.WriteLine("Будние дни:( ");
+    return;
+}
+if (Num == 6 || Num == 7)
+{
+    Console.WriteLine("Выходные ИИИИУУУУУ ");
+    return;
+}
+if (Num > 7)
+{
+    Console.WriteLine("В неделе пока что ещё 7 дней:3");
+    return;
+}
+if (Num < 0 )
+{
+    Console.WriteLine("Ого, 0 дней в неделе, а жить когда?");
+    return;
+}
+if (Num == 5)
+{
+    Console.WriteLine("О, ЕБАТЬ, ПЯТНИЦА, ПОРА НАХУЯРИТЬСЯ");
+    return;
+}
